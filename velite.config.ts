ae02ac12afc3bcdd5dabe4c48bd868916a27cb64
dclassicgenius <patamaechi1@gmail.com>
2025-01-18 17:43:19 +0300

    implement last modified date for posts

--- velite.config.ts
+++ velite.config.ts
@@ -4,6 +4,8 @@ import rehypePrettyCode from "rehype-pretty-code";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeToc from "@stefanprobst/rehype-extract-toc";
 import rehypeTocExtract from "@stefanprobst/rehype-extract-toc/mdx";
+import { promisify } from "util";
+import { exec } from "child_process";
 
 const computedFields = <
   T extends { slug: string; meta: { readingTime: number } }
@@ -15,6 +17,31 @@ const computedFields = <
   readingTime: data.meta.readingTime,
 });
 
+const execAsync = promisify(exec);
+
+const getGitLastModifiedDate = () =>
+  s
+    .custom<string | undefined>((i) => i === undefined || typeof i === "string")
+    .transform<string>(async (value, { meta, addIssue }) => {
+      try {
+        if (value != null) {
+          addIssue({
+            fatal: false,
+            code: "custom",
+            message:
+              "`s.timestamp()` schema will resolve the value from `git log -1 --format=%cd`",
+          });
+        }
+
+        const { stdout } = await execAsync(
+          `git log -1 --format=%cd -- "${meta.path}"`
+        );
+        return new Date(stdout).toISOString();
+      } catch {
+        return new Date().toISOString();
+      }
+    });
+
 const posts = defineCollection({
   name: "Post",
   pattern: "posts/**/*.mdx",
@@ -24,7 +51,7 @@ const posts = defineCollection({
       title: s.string().max(99),
       description: s.string().max(999).optional(),
       date: s.isodate(),
-      updatedOn: s.isodate().optional(),
+      updatedOn: getGitLastModifiedDate(),
       published: s.boolean().default(true),
       tags: s.array(s.string()).optional(),
       image: s.string().optional(),
